649b84608c2a58007d5e5fe745f6ab9aab732d03
Alex <67673258+alexnehc@users.noreply.github.com>
2024-02-07 11:00:54 -0500

    comments

--- cs361-main/main/week2/Main.java
+++ cs361-main/main/week2/Main.java
@@ -14,18 +14,21 @@ public class Main {
     }
 
     static void generate(int start, int range, int amount, int i, int[] combos){
+        //base case: if index == amount of elements required, simply print combination array
         if (i == amount){
             printAll(combos);
             return;
         }
 
-
+        //else recursively call the generate function to print all the different combinations as stated by the question.
         for (int j = start; j <= range; j++){
             combos[i] = j;
             generate(j + 1, range, amount, i + 1, combos);
         }
     }
 
+
+    //function to print all of the numbers out.
     static void printAll(int[] combos){
         for (int i : combos){
             System.out.print(i + " ");

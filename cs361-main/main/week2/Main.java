20bffebe9c2c84e1e5fa3030c51618cf2d9d9a5a
Alex <67673258+alexnehc@users.noreply.github.com>
2024-02-07 10:57:23 -0500

    Homework 2

--- cs361-main/main/week2/Main.java
+++ cs361-main/main/week2/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main (String args[]){
-        System.out.println("Hello world");
+
+        Scanner scan = new Scanner(System.in);
+        int range = scan.nextInt(); // the range of elements
+        int amountOfElements = scan.nextInt(); // the amount of different combinations required. (k)
+
+        //array of combinations
+        int[] combos = new int[amountOfElements];
+        generate(1, range, amountOfElements, 0, combos);
+
     }
+
+    static void generate(int start, int range, int amount, int i, int[] combos){
+        if (i == amount){
+            printAll(combos);
+            return;
+        }
+
+
+        for (int j = start; j <= range; j++){
+            combos[i] = j;
+            generate(j + 1, range, amount, i + 1, combos);
+        }
+    }
+
+    static void printAll(int[] combos){
+        for (int i : combos){
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+    }
+
 }
